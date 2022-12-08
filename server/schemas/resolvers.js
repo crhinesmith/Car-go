67fb4b060d73412267ba611c5a28688e5ebf7da2
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-12-07 21:39:33 -0500

    resolvers and typeDefs setup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,14 @@
+const resolvers = {
+  Query: {
+    car: async () => {
+      // Populate the classes and professor subdocuments when querying for schools
+      return await Car.find()
+    },
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  }
+};
+
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,26 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+  }
 
+  type Car {
+    name: String
+  }
+
+  // type Query {
+  //   car: [Profile]!
+  //   user(profileId: ID!): User
+  // }
+
+  type Mutation {
+    adduser(name: String!): User
+    addCar(userId: ID!, car: String!): Car
+    removeProfile(profileId: ID!): Profile
+    removeSkill(profileId: ID!, skill: String!): Profile
+  }
+`;
 
 module.exports = typeDefs;
