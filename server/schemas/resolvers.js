42c0aebb79f8b02b375595ed68289304878fe188
Emilio Acosta-Galicia <103851807+EmilioAcostaG@users.noreply.github.com>
2022-12-08 21:50:08 -0500

    Server schemas (#10)
    * resolvers and typeDefs setup
    
    * added .gitignore for PR
    
    * delete node modules
    
    * delete more node modules
    
    * delete files for PR
    
    * update resolvers.js
    
    * add and update .gitignore file
    
    * save work
    
    * resolve issue
    
    * update branch .env
    
    * update resolvers
    
    Co-authored-by: crhinesmith <107893624+crhinesmith@users.noreply.github.com>

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User, Product, Category, Order, Car } = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
@@ -14,5 +14,18 @@ const resolvers = {
     user: async (parent, { userId }) => {
       return User.findOne({ _id: userId });
     },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+  },
+    addCarToWatchlist: async (parent, {carId}, context) => {
+      if (context.user) {
+      return await User.findByIdAndUpdate(context.user._id, { $push: { watching: carId } });
+    }
+   throw new AuthenticationError('Not logged in');
   }
-};
+}};
