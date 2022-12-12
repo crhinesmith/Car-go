92c79b6ea88b452fa564a6d9236e3d9ed9e2c857
Horatious Harris <108444082+geekcoldhand@users.noreply.github.com>
2022-12-12 14:58:19 -0500

    fixed database seeding now testing data in apollo (#13)
    Co-authored-by: git geek <cout.1.endl@gmail.com>

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,19 +1,18 @@
-const db = require('./connectiononnection');
-const { Car, User } = require('../models');
-const carSeeds = require('./carSeeds.json');
-const userSeeds = require('./userSeeds.json');
+const db = require("./connection");
+const { Car, User } = require("../models");
+const carSeeds = require("./carSeeds.json");
+const userSeeds = require("./userSeeds.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
     await Car.deleteMany({});
     await Car.create(carSeeds);
     await User.deleteMany({});
     await User.create(userSeeds);
 
-    console.log('User and Car data seeded!');
+    console.log("User and Car data seeded!");
     process.exit();
   } catch (err) {
     throw err;
   }
 });
-
