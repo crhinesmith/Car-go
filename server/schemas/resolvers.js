6417df5bc82101d0704b63172c86544b8d8afd40
ripbog <crhinesmith5@gmail.com>
2022-12-12 19:36:14 -0500

    working typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,28 +9,83 @@ const resolvers = {
     users: async () => {
       return User.find();
     },
-    user: async (parent, { userId }) => {
-      return User.findOne({ _id: userId }).populate('watching');
+    user: async (parent, { _id  }) => {
+      return User.findOne({ _id }).populate('watching');
+    },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'orders.products',
+          populate: 'category'
+        });
+
+        return user.orders.id(_id);
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
     cars: async () => {
       return await Car.find();
     },
-    car: async (parent, { carId }) => {
-      return await Car.findOne({ _id: carId });
+    car: async (parent, { _id }) => {
+      return await Car.findOne({ _id });
     },
-    carMake: async (parent, { carMake }) => {
-      return await Car.find({ make: carMake });
+    carMake: async (parent, { make }) => {
+      return await Car.find({ make });
     },
-    carModel: async (parent, { carModel }) => {
-      return await Car.find({ model: carModel });
+    carModel: async (parent, { model }) => {
+      return await Car.find({ model });
     },
-    carYear: async (parent, { carYear }) => {
-      return await Car.find({ year: carYear });
+    carYear: async (parent, { year }) => {
+      return await Car.find({ year });
     },
+    carsByMileage: async (parent, { minimumMileage, maximumMileage }) => {
+      return await Car.find({ 
+        mileage: {
+          $gt: minimumMileage,
+          $lt: maximumMileage
+        }
+      });
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ car: args.car });
+      const line_items = [];
+
+      const { singleCar } = await order.populate('car');
+
+        const car = await stripe.products.create({
+          name: "Car",
+          description: `${singleCar.make} ${singleCar.model}`,
+          
+        });
+
+        const price = await stripe.prices.create({
+          product: car.id,
+          unit_amount: singleCar.price,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
   },
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, {input}) => {
+      const user = await User.create({input});
       const token = signToken(user);
 
       return { token, user };
@@ -67,10 +122,14 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    }
+    },
+    carSold: async (parent, { carId }) => {
+      return await Car.findByIdAndDelete({ carId });
+    },
   },
 };
 
 module.exports = resolvers;
 
 
+ 
